690882cbb6654380b4855b7f0f5beff16b64d5a6
email <obansal.kunalo@gmail.com>
2025-03-21 13:06:52 +0530

    Web analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { Toaster } from "@/components/ui/toaster"
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" })
 
